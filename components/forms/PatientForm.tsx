ee4f8c5f7869901d33e306d40e215064465445ed
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2024-12-04 01:43:44 +0530

    upload

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -72,7 +72,7 @@ export const PatientForm = () => {
           control={form.control}
           name="email"
           label="Email"
-          placeholder="johndoe@gmail.com"
+          placeholder="ayushtiwari@gmail.com"
           iconSrc="/assets/icons/email.svg"
           iconAlt="email"
         />
