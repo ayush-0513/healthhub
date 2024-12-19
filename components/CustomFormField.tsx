b9a8c0880c41a32e657189f51cbe722803e9ade3
[ayush tiwari] <[ayush0511tiwari@gmail.com]>
2024-12-19 13:13:05 +0530

    commit

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -119,14 +119,15 @@ const RenderInput = ({ field, props }: { field: any; props: CustomProps }) => {
             <ReactDatePicker
               showTimeSelect={props.showTimeSelect ?? false}
               selected={field.value}
-              onChange={(date: Date) => field.onChange(date)}
+              onChange={(date: Date | null) => field.onChange(date)} // Handle null
               timeInputLabel="Time:"
               dateFormat={props.dateFormat ?? "MM/dd/yyyy"}
               wrapperClassName="date-picker"
             />
           </FormControl>
+
         </div>
-      );
+      ); 
     case FormFieldType.SELECT:
       return (
         <FormControl>
